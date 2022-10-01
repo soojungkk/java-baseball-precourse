0e4ae4c79789b0f6bfbca4a36775b8d8ee824ed8
soojungkk <sj921101@naver.com>
2022-10-02 00:09:07 +0900

    test class init

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        //System.out.println(s);
     }
 }
